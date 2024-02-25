33e69e292e908cd177372985992837b86eb9b0c1
Mounsef-Hachem <mounsefhachem97@gmail.com>
2024-02-25 02:28:13 +0100

    Add mvn profiles and modules

---
+++ book-store-commun/src/main/java/com/bookstorecommun/bookstorecommun/BookStoreCommunApplication.java
@@ -0,0 +1,13 @@
+package com.bookstorecommun.bookstorecommun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookStoreCommunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookStoreCommunApplication.class, args);
+	}
+
+}
---
+++ book-store-commun/src/test/java/com/bookstorecommun/bookstorecommun/BookStoreCommunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookstorecommun.bookstorecommun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookStoreCommunApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
