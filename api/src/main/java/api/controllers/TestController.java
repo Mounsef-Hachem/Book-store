22c42d6711db25c85be7e53555375b1ee458229d
Mounsef-Hachem <mounsefhachem97@gmail.com>
2024-03-04 23:45:22 +0100

    Update

--- api/src/main/java/api/controllers/TestController.java
+++ api/src/main/java/api/controllers/TestController.java
@@ -1,15 +1,20 @@
 package api.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import common.service.TestService;
 
 @RestController
 public class TestController {
 
+	@Autowired
+	private TestService service;
 	
 	@GetMapping("/")
 	String helloController() {
-		return "tooooooo";
+		return service.sayHello();
 	}
 	
 	@GetMapping("/test")
